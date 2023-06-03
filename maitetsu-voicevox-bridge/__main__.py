f94fde0cd057ef72512a270843580715e538484c
uluvtu <28953019+uluvtu@users.noreply.github.com>
2023-06-04 06:54:00 +0900

    volume,speed,api_base_urlを追加、audio_queryの生成時のスピーカーIDを入力値に変更

--- maitetsu-voicevox-bridge/__main__.py
+++ maitetsu-voicevox-bridge/__main__.py
@@ -12,11 +12,19 @@ async def main():
     parser.add_argument(
         "-o", "--output", type=PureWindowsPath, help="output path", default="output.wav"
     )
+    parser.add_argument("--speed", type=float, help="reading speed", default=None)
+    parser.add_argument("--volume", type=float, help="voice volume", default=None)
+    parser.add_argument("--api_base_url",type=str, help="VOICEVOX API url", default="http://localhost:50021")
     parser.add_argument("word", type=str, help="read out sentence")
     args = parser.parse_args()
 
-    async with Client() as client:
-        audio_query = await client.create_audio_query(args.word, speaker=1)
+    
+    async with Client(args.api_base_url) as client:
+        audio_query = await client.create_audio_query(args.word, speaker=args.speaker)
+        if args.speed is not None:
+            audio_query.speed_scale=args.speed
+        if args.volume is not None:
+            audio_query.volume_scale=args.volume
         with open(args.output, "wb") as f:
             f.write(await audio_query.synthesis(speaker=args.speaker))
 
